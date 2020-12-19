7f6842a4283155ade985f69bcce87f931dc48027
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-12-19 12:37:41 +0600

    consts file was added

---
+++ consts.py
@@ -0,0 +1,3 @@
+URL = "https://hh.kz/search/vacancy?area=159&fromSearchLine=true&st=searchVacancy&text=python"
+minimum_salary = 0
+avoid_words = ["преподаватель"]
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from consts import *
 from kzt_exchangerates import Rates
 from selenium.common import exceptions
 from selenium import webdriver
@@ -13,6 +14,22 @@ def get_only_digits(array):
     return 0
 
 
+def vacancy_processing(v):
+    name = v.find_element_by_tag_name("a").text
+    for word in avoid_words:
+        if word in name.lower():
+            return
+    salary_info = v.find_element_by_class_name("vacancy-serp-item__sidebar").text
+    if not salary_info:
+        # salary = "No information"
+        return
+    salary = Salary(salary_info)
+    if salary.kzt < minimum_salary:
+        return
+    print(f"Название:   {name}")
+    print(f"Оклад:      {salary}\n")
+
+
 class Salary:
     top_value = 0
     bot_value = 0
@@ -24,10 +41,10 @@ class Salary:
         if len(info) == 1:
             # TODO check "от" and "до"
             self.strict_parse(info[0].split())
-            self.kzt = self.exchange(self.top_value)
+            self.kzt = self.to_kzt(self.top_value)
         else:
             self.range_parse(info)
-            self.kzt = self.exchange(self.bot_value)
+            self.kzt = self.to_kzt(self.bot_value)
 
     def __str__(self):
         if not self.bot_value:
@@ -37,7 +54,7 @@ class Salary:
         else:
             output = f"{self.bot_value} - {self.top_value} {self.currency}"
             if self.currency != "KZT":
-                output += f" ({self.kzt} - {self.exchange(self.top_value)} в KZT)"
+                output += f" ({self.kzt} - {self.to_kzt(self.top_value)} в KZT)"
         return output
 
     def strict_parse(self, info):
@@ -55,33 +72,24 @@ class Salary:
         # second is top
         self.strict_parse(info[1].split())
 
-    def exchange(self, value):
+    def to_kzt(self, value):
         # TODO try to find different way
         return value * round(rates.get_exchange_rate(self.currency, 'KZT'))
 
 
 rates = Rates()
-url = "https://hh.kz/search/vacancy?area=159&fromSearchLine=true&st=searchVacancy&text=python"
 options = webdriver.ChromeOptions()
 options.headless = True
 driver = webdriver.Chrome("chromedriver.exe", options=options)
 
-driver.get(url)
+driver.get(URL)
 table = driver.find_element_by_class_name("vacancy-serp")
-# TODO refactor search
+# TODO refactor search. maybe using css selector
 vacancies = table.find_elements_by_xpath("//*[@class='vacancy-serp-item__row vacancy-serp-item__row_header']")
 
 for vacancy in vacancies:
     try:
-        name = vacancy.find_element_by_tag_name("a").text
-        if "преподаватель" in name.lower():
-            continue
-        salary = vacancy.find_element_by_class_name("vacancy-serp-item__sidebar").text
-        if not salary:
-            # salary = "No information"
-            continue
-        print(f"Название:\t{name}")
-        print(f"Оклад:\t{Salary(salary)}")
+        vacancy_processing(vacancy)
     except exceptions.NoSuchElementException:
         pass
 driver.quit()
