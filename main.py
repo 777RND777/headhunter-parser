7a5b903b8f12cdba2b2b200ef935dd0b603bbd53
Nurmukhanbet Rakhimbayev <777rnd777@github.com>
2021-07-07 00:28:37 +0600

    salary bug was fixed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from config import *
-from kzt_exchangerates import Rates
-from selenium.common import exceptions
-from selenium import webdriver
 from concurrent.futures import ThreadPoolExecutor
+from kzt_exchangerates import Rates
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import config
 import re
 
 
@@ -31,23 +31,27 @@ class Salary:
             salary_range = [int(i.replace(" ", "")) for i in re.findall(r"\d+", info)]
             if not salary_range:
                 return
+
         if len(salary_range) == 1:
             self.top_value = salary_range[0]
             self.kzt = self.to_kzt(self.top_value)
-        else:
-            self.bot_value = salary_range[0]
-            self.top_value = salary_range[1]
-            self.kzt = self.to_kzt(self.bot_value)
+            return
+
+        # actual range
+        self.bot_value = salary_range[0]
+        self.top_value = salary_range[1]
+        self.kzt = self.to_kzt(self.bot_value)
 
     def __str__(self):
-        if not self.bot_value:
-            output = f"{self.top_value:,} {self.currency}"
+        if not self.top_value:
+            output = f"{self.bot_value:,} {self.currency}"
             if self.currency != "KZT":
                 output += f" ({self.kzt:,} в KZT)"
-        else:
-            output = f"{self.bot_value:,} - {self.top_value:,} {self.currency}"
-            if self.currency != "KZT":
-                output += f" ({self.kzt:,} - {self.to_kzt(self.top_value):,} в KZT)"
+            return output
+
+        output = f"{self.bot_value:,} - {self.top_value:,} {self.currency}"
+        if self.currency != "KZT":
+            output += f" ({self.kzt:,} - {self.to_kzt(self.top_value):,} в KZT)"
         return output
 
     def to_kzt(self, value):
@@ -57,15 +61,18 @@ class Salary:
 def vacancy_processing(v):
     link = v.find_element_by_tag_name("a")
     name = link.text
-    for word in AVOID_WORDS:
+    for word in config.AVOID_WORDS:
         if word in name.lower():
             return
+
     salary_info = v.find_element_by_class_name("vacancy-serp-item__sidebar").text
     if not salary_info:
         return
+
     salary = Salary(salary_info)
-    if salary.kzt < MINIMUM_SALARY:
+    if salary.kzt < config.MINIMUM_SALARY:
         return
+
     print(f'''
 Название:   {name}
 Оклад:      {salary}
@@ -84,12 +91,12 @@ def page_collector(url):
     with ThreadPoolExecutor() as executor:
         try:
             executor.map(vacancy_processing, table.find_elements_by_css_selector("div.vacancy-serp-item"))
-        except exceptions.NoSuchElementException:
+        except NoSuchElementException:
             pass
 
     try:
         get_next_page()
-    except exceptions.NoSuchElementException:
+    except NoSuchElementException:
         pass
 
 
@@ -98,5 +105,5 @@ if __name__ == "__main__":
     options = webdriver.ChromeOptions()
     options.headless = True
     driver = webdriver.Chrome("chromedriver.exe", options=options)
-    page_collector(START_PAGE)
+    page_collector(config.START_PAGE)
     driver.quit()
